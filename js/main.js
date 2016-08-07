253d9503e004968fc24f9b18ecd1e3421724afe7
wwwooo <malinovsvska@gmail.com>
2016-08-07 15:10:48 +0300

    update main.js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,39 @@
 "use strict";
 
 var pow = function() {
-  var a = prompt('enter an integer');
-  var x = prompt('enter an integer');
-  var check = !isNaN(parseFloat(a)) && !isNaN(parseFloat(x)) && (Math.ceil(a) == a) && (Math.ceil(x) == x);
+    var a = prompt('enter an integer');
+    var x = prompt('enter an integer');
+    var check = !isNaN(parseFloat(a)) && !isNaN(parseFloat(x)) && (Math.ceil(a) == a) && (Math.ceil(x) == x);
 
-  if (check) {
-    var result = 1;
-    if (x < 0) {
-      while (x++ < 0) {
-        result = result * a;
-      }
-      result = 1 / result;
+    if (check) {
+        var result = 1;
+        if (x < 0) {
+            x = -x;
+            a = 1 / a;
+        }
+        for (var i = 0; i < x; i++) {
+            result *= a;
+        }
+        console.log('result = ' + result);
     } else {
-      while (x-- > 0) {
-        result = result * a;
-      }
+        console.log('not correct');
     }
-    console.log(result);
-  } else {
-    console.log('not correct');
-  }
 };
 
 var signIn = function() {
-  var arr = [];
-  while (arr.length < 5) {
-    var name = prompt('enter name');
-    arr.push(name);
-  }
-
-  var userName = prompt('enter the user name');
-  for (var i = 0; i < 5; i++) {
-    if (arr[i] === userName) {
-      return alert(userName + ', вы успешно вошли');
+    var arr = [];
+    while (arr.length < 5) {
+        var name = prompt('enter name');
+        arr.push(name);
     }
-  }
-  return alert('login failed');
+
+    var userName = prompt('enter the user name');
+    for (var i = 0; i < 5; i++) {
+        if (arr[i] === userName) {
+            return alert(userName + ', вы успешно вошли');
+        }
+    }
+    return alert('login failed');
 };
 
 alert('function pow');
